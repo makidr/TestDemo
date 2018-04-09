92ea583f57d3014e6962d62ed6391940d895cfc3
makidr <betterdr07@163.com>
2018-04-09 18:15:38 +0800

    Initial commit

---
+++ src/Test/Test1.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class Test1
+{
+
+	public static void main(String[] args)
+	{
+		String a = "这是个例子！！！"; // 这句话意思是： 我们规定好 a 这个字 代表了"这是个例子！！！" 这句话
+		System.out.println(a); //
+	}
+
+}
