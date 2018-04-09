ae1d77c55f79a3348ecd660b728c25089be40e53
makidr <betterdr07@163.com>
2018-04-09 18:21:26 +0800

    dr

--- src/Test/Test1.java
+++ src/Test/Test1.java
@@ -5,8 +5,8 @@ public class Test1
 
 	public static void main(String[] args)
 	{
-		String a = "这是个例子！！！"; // 这句话意思是： 我们规定好 a 这个字 代表了"这是个例子！！！" 这句话
-		System.out.println(a); //
+		String a = "这是个例子！！！";
+		System.out.println(a);
 	}
 
 }
